0a334ad13ade80f3e58cbbbed26249d098d80563
Victor Penso <vic.penso@gmail.com>
2018-07-09 11:44:45 +0200

    local prometheus images

--- var/aliases/prometheus.sh
+++ var/aliases/prometheus.sh
@@ -6,12 +6,17 @@ export PROMETHEUS_VERSION \
 
 echo 'prometheus-docker-images-to-local-registry() -- Copy Prometheus container images to local registry'
 prometheus-docker-images-to-local-registry() {
+        
         echo Pull Prometheus container images from DockerHub
         docker pull prom/prometheus:$PROMETHEUS_VERSION
         docker pull prom/node-exporter:$PROMETHEUS_NODE_EXPORTER_VERSION
+        
         echo Tag and push Prometheus images to local registry
-        docker tag prometheus:$PROMETHEUS_VERSION $DOCKER_REGISTRY lxcm01:5000/prometheus
-        docker tag node-exporter:$PROMETHEUS_NODE_EXPORTER_VERSION lxcm01:5000/node-exporter 
-        docker push lxcm01:5000/prometheus
-        docker push lxcm01:5000/node-exporter
+        docker tag prom/prometheus:$PROMETHEUS_VERSION \
+                   lxcm01:5000/prometheus:$PROMETHEUS_VERISON
+        docker tag prom/node-exporter:$PROMETHEUS_NODE_EXPORTER_VERSION \
+                   lxcm01:5000/node-exporter:$PROMETHEUS_NODE_EXPORTER_VERSION 
+        docker push lxcm01:5000/prometheus:$PROMETHEUS_VERSION
+        docker push lxcm01:5000/node-exporter:$PROMETHEUS_NODE_EXPORTER_VERSION
+
 }
